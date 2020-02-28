eb90ae468e3f25f5a90c52adaac9657bf434fa47
jasonwhk <jason19888@gmail.com>
2020-02-28 16:01:41 +0000

    changed default level

--- mpikat/meerkat/fbfuse/fbfuse_product_controller.py
+++ mpikat/meerkat/fbfuse/fbfuse_product_controller.py
@@ -1112,7 +1112,7 @@ class FbfProductController(object):
             self._previous_sb_config = sb_config
             self._state_sensor.set_value(self.READY)
             self.log.info("Successfully prepared FBFUSE product")
-        yield self.set_levels(20.0, 4.0)
+        yield self.set_levels(20.0, 10.0)
         yield self._activity_tracker.start()
 
     @coroutine
